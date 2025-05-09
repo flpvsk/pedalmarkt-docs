451255c155a2bd3bb4eb455ef31edb815ca44070
flpvsk <filipovskii.off@gmail.com>
2025-05-09 22:02:00 +0200

    hypno

---
+++ hypno-docs/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# osascript -e 'quit app "Preview"'
+
+rm -r build
+mkdir build
+pdflatex -halt-on-error -output-directory=build index.tex
+pdflatex -halt-on-error -output-directory=build index.tex
+# open build/index.pdf
